253c4d389949dc93aec5ec964685b89e3aa41476
aflouret <aflouret@gmail.com>
2021-02-11 17:06:24 -0300

    Cambios en catalogo

--- 09-TusLibros/main.py
+++ 09-TusLibros/main.py
@@ -1,19 +1,17 @@
 import unittest
 
+
 class Cart:
 
-    def __init__(self,aClientId,aPassword,aCatalog):
-        self.clientId = aClientId
-        self.password = aPassword
+    def __init__(self):
         self.quantity = 0
         self.books = {}
-        self.catalog = aCatalog
 
     def isEmpty(self):
         return self.quantity == 0
 
-    def addToCart(self, aBook, aBookQuantity = 1):
-        if self.catalog.hasBook(aBook) == False:
+    def addToCart(self, aBook, catalog, aBookQuantity = 1):
+        if catalog.hasBook(aBook) == False:
             raise Exception("Book not in catalog")
 
         self.books[aBook] = aBookQuantity
@@ -25,12 +23,6 @@ class Cart:
     def quantityOfBook(self, aBook):
         return self.books[aBook]
 
-    """def listCart(self):
-        list = "0"
-        for book, quantity in self.books.items():
-            list += "|" + book
-            list += "|" + str(quantity)
-        return list"""
 
 class Catalog:
 
@@ -44,58 +36,51 @@ class Catalog:
 class TestTusLibros(unittest.TestCase):
 
     def test1_newCartIsEmpty(self):
-        catalog = Catalog(["Un libro", "Otro libro"])
-        newCart = Cart("100","abc123", catalog)
+        newCart = Cart()
         self.assertTrue(newCart.isEmpty())
 
-    def test2_aValidBookCanBeAddedToCartCorrectly(self):
+    def test2_aBookInCatalogCanBeAddedToCart(self):
         catalog = Catalog(["Un libro", "Otro libro"])
-        newCart = Cart("100", "abc123", catalog)
+        newCart = Cart()
         aBook = "Un libro"
-        newCart.addToCart(aBook)
+        newCart.addToCart(aBook, catalog)
         self.assertFalse(newCart.isEmpty())
         self.assertTrue(newCart.hasBook(aBook))
 
-    def test3_anInvalidBookIsNotAddedToCart(self):
+    def test3_aBookNotInCatalogCanNotBeAddedToCart(self):
         catalog = Catalog(["Un libro", "Otro libro"])
-        newCart = Cart("100", "abc123", catalog)
+        newCart = Cart()
         aBook = "Un libro que no esta"
 
         with self.assertRaises(Exception):
-            newCart.addToCart(aBook)
+            newCart.addToCart(aBook, catalog)
         self.assertTrue(newCart.isEmpty())
         self.assertFalse(newCart.hasBook(aBook))
 
     def test4_canAddManyBooksOfSameTitle(self):
         catalog = Catalog(["Un libro", "Otro libro"])
-        newCart = Cart("100", "abc123", catalog)
+        newCart = Cart()
         aBook = "Un libro"
-        newCart.addToCart(aBook, 2)
+        newCart.addToCart(aBook, catalog, 2)
         self.assertFalse(newCart.isEmpty())
         self.assertTrue(newCart.hasBook(aBook))
         self.assertEqual(newCart.quantity, 2)
 
     def test5_canAddManyDifferentBooks(self):
         catalog = Catalog(["Un libro", "Otro libro"])
-        newCart = Cart("100", "abc123", catalog)
+        newCart = Cart()
         aBook = "Un libro"
         anotherBook = "Otro libro"
-        newCart.addToCart(aBook, 2)
-        newCart.addToCart(anotherBook, 3)
+        newCart.addToCart(aBook, catalog, 100)
+        newCart.addToCart(anotherBook, catalog, 100)
         self.assertFalse(newCart.isEmpty())
-        self.assertTrue(newCart.quantityOfBook(aBook) == 2)
-        self.assertTrue(newCart.quantityOfBook(anotherBook) == 3)
-        self.assertEqual(newCart.quantity, 5)
-
-
-
+        self.assertEqual(newCart.quantityOfBook(aBook), 100)
+        self.assertEqual(newCart.quantityOfBook(anotherBook), 100)
+        self.assertEqual(newCart.quantity, 200)
 
 
 
 def main():
     unittest.main()
-
-
-    return 0
 
 main()
